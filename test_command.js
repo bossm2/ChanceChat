9a76a7a31f87cd5491c5a040ea671c9b60813ed5
bossm2 <im_one_man@yahoo.com>
2019-07-26 07:48:07 -0700

    Update test_command.js

--- test_command.js
+++ test_command.js
@@ -285,7 +285,7 @@ function get_tmp() {
 		console.log(atmp)
 	});
 }
-get_tmp()
+//get_tmp()
 function save_tmp() {
 	request({
 		url: ("https://api.github.com/repos/bossm3/filing/contents/mig.txt"),
