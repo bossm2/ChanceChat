bce240b42c15988c45b804fd6d831dd8f2258c96
bossm2 <im_one_man@yahoo.com>
2019-08-04 10:21:58 -0700

    c

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const { btoken, stoken, s2token, ttoken, welcometitle, canceltitle, blocktitle, counttitle, waittitle, starttitle, notsupportsmg, gtoken, settingtitle, isreptitle, isbloctitle, helptitle,key } = require("./Constant.js");
 //gggggggggggggggggggggggggggggg
+//aaaaaaaaaaaaaaaaaaaaaaaaaa
 //#region variables
 var EventSource = require("eventsource");
 var request = require('requestretry');
